cb605a4eb59a8d9b388826784333dcd3deb23771
Stephen Senft <stephensenft21@gmail.com>
2019-03-18 11:38:11 -0500

    first commit

---
+++ src/scripts/ApiMGR.js
@@ -0,0 +1,10 @@
+const nationalParksCall = () => {
+       return fetch(`http://localhost:9099/parks`)
+            .then(response => response.json()  
+        }
+
+        nationalParksCall("hello")
+        console.log(nationalParksCall)
+
+        
+        

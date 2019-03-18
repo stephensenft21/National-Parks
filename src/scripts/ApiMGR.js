a68cc0f817ab34c144eb9874991c23a5bd69e256
Stephen Senft <stephensenft21@gmail.com>
2019-03-18 15:50:51 -0500

    added styling to css made DOM mgr && API mgr changes

--- src/scripts/ApiMGR.js
+++ src/scripts/ApiMGR.js
@@ -1,10 +1,10 @@
 const nationalParksCall = () => {
-       return fetch(`http://localhost:9099/parks`)
-            .then(response => response.json()  
-        }
+    return fetch("http://localhost:9099/parks")
+        .then(response => response.json())
+        .then(parks =>  {
+            renderParks(parks)
 
-        nationalParksCall("hello")
-        console.log(nationalParksCall)
+        });
+};
 
-        
-        
+nationalParksCall()
--- src/scripts/DomMGR.js
+++ src/scripts/DomMGR.js
@@ -1,4 +1,29 @@
-const nationalParks = () =>{
+const createNationalParksVisited = (parkObject) => {
+    return `
+        <article class="true">
+            <h3>${parkObject.name}</h3>
+            <p>${parkObject.state}</p>
 
+        </article>
+    `
+}
+const createNationalParksNotVisited = (parkObject) => {
+    return `
+        <article class="false">
+            <h3>${parkObject.name}</h3>
+            <p>${parkObject.state}</p>
+
+        </article>
+    `
 
 }
+
+
+const renderParks = parks => {
+ parks.foreach(parkObject => {})
+ if (parkObjects)
+}
+
+// const nationalParksContainer = document.querySelector("#display-container")
+// nationalParksContainer.innerHTML = createNationalParks()
+
